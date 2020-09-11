629e60c33ec6135913e4a4dd4107773b0a56bc98
Farzad <farzadsole@gmail.com>
2020-09-11 18:01:12 +0100

    Updates

---
+++ precache-manifest.085227526145cbf2fecbaab74c62b55f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69698b9414c9948019e1254576f553c",
+    "url": "/Test1/index.html"
+  },
+  {
+    "revision": "6db31bd0eb3554ebcd53",
+    "url": "/Test1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b3287c65a36681af3eb",
+    "url": "/Test1/static/js/2.fa9dc631.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Test1/static/js/2.fa9dc631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db31bd0eb3554ebcd53",
+    "url": "/Test1/static/js/main.a9a1fe95.chunk.js"
+  },
+  {
+    "revision": "614914637e9fdd74ad25",
+    "url": "/Test1/static/js/runtime-main.d99f5707.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Test1/static/media/logo.5d5d9eef.svg"
+  }
+]);
